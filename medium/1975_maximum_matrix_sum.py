3d75c4b9771cd4576d6d6bf00f2b07d6658f39d0
novanjanis322 <novangfc@gmail.com>
2026-01-05 23:40:00 +0700

    Week 2 Day 2: Maximum Matrix Sum - greedy approach

---
+++ medium/1975_maximum_matrix_sum.py
@@ -0,0 +1,56 @@
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# You are given an n x n integer matrix. You can do the following operation any number of times:
+
+# Choose any two adjacent elements of matrix and multiply each of them by -1.
+# Two elements are considered adjacent if and only if they share a border.
+
+# Your goal is to maximize the summation of the matrix's elements. Return the maximum sum of the matrix's elements using the operation mentioned above.
+
+
+# Example 1:
+
+
+# Input: matrix = [[1,-1],[-1,1]]
+# Output: 4
+# Explanation: We can follow the following steps to reach sum equals 4:
+# - Multiply the 2 elements in the first row by -1.
+# - Multiply the 2 elements in the first column by -1.
+# Example 2:
+
+
+# Input: matrix = [[1,2,3],[-1,-2,-3],[1,2,3]]
+# Output: 16
+# Explanation: We can follow the following step to reach sum equals 16:
+# - Multiply the 2 last elements in the second row by -1.
+
+
+# Constraints:
+
+# n == matrix.length == matrix[i].length
+# 2 <= n <= 250
+# -105 <= matrix[i][j] <= 105
+
+# Link: https://leetcode.com/problems/maximum-matrix-sum/
+
+from typing import List
+
+
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        summarize = 0
+        neg_total = 0
+        min_abs = float("inf")
+        for row in matrix:
+            for col in row:
+                if col < 0:
+                    neg_total += 1
+                min_abs = min(min_abs, abs(col))
+                summarize += abs(col)
+        if neg_total % 2 == 0:
+            return summarize
+        else:
+            return summarize - (2 * min_abs)
